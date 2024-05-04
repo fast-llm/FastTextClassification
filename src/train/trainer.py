272d5df91eafc921fd856b1477926e934475c707
AmbroseX <rongkangxiong@idea.edu.cn>
2024-05-04 18:55:40 +0800

    add trainer

---
+++ src/train/trainer.py
@@ -0,0 +1,19 @@
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
+
+import torch
+from transformers import PreTrainedModel
+
+from extras.callbacks import LogCallback
+from hparams.parser import get_train_args
+from train.workflow import run_train
+
+
+if TYPE_CHECKING:
+    from transformers import TrainerCallback
+
+
+
+def run_exp(args: Optional[Dict[str, Any]] = None, callbacks: Optional[List["TrainerCallback"]] = None):
+    model_args, data_args, training_args = get_train_args(args)
+    callbacks = [LogCallback()] if callbacks is None else callbacks
+    run_train(model_args, data_args, training_args, callbacks)
