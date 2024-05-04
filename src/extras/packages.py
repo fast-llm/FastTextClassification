b07561c90b4e9447121a9b47ac46ebb81ce66f6d
AmbroseX <rongkangxiong@idea.edu.cn>
2024-05-04 18:56:31 +0800

    compare version

--- src/extras/packages.py
+++ src/extras/packages.py
@@ -1,6 +1,6 @@
 import importlib.metadata
 import importlib.util
-
+from distutils.version import LooseVersion
 
 def _is_package_available(name: str) -> bool:
     return importlib.util.find_spec(name) is not None
@@ -11,6 +11,25 @@ def _get_package_version(name: str) -> str:
         return importlib.metadata.version(name)
     except Exception:
         return "0.0.0"
+
+def compare_versions(version1, version2):
+    """比较两个版本号字符串的大小。
+
+    Args:
+        version1 (str): 第一个版本号字符串。
+        version2 (str): 第二个版本号字符串。
+    Returns:
+        int: 如果version1大于version2，则返回1；如果version1小于version2，则返回-1；如果version1等于version2，则返回0。
+    """
+    if LooseVersion(version1) > LooseVersion(version2):
+        return 1
+    elif LooseVersion(version1) < LooseVersion(version2):
+        return -1
+    else:
+        return 0
+
+def get_transformer_version():
+    return _get_package_version('transformers')
 
 def is_fastapi_availble():
     return _is_package_available("fastapi")
