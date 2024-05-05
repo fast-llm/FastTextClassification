d3da7d58de748941675fa7629a245f5b62fa5fa7
AmbroseX <rongkangxiong@idea.edu.cn>
2024-05-05 22:45:41 +0800

    update deepspeed

--- scripts/build-env-121.sh
+++ scripts/build-env-121.sh
@@ -11,14 +11,14 @@ conda install https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/li
 conda install https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/linux-64/cudnn-8.9.7.29-hcdd5f01_2.conda
 conda install -c nvidia cuda-compiler
 
-
-
 # https://pytorch.org/get-started/previous-versions/
 # xformers版本地址: https://github.com/facebookresearch/xformers/tags
 pip install torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 --index-url https://download.pytorch.org/whl/cu121
 # https://github.com/huggingface/transformers/tags
+
 pip install poetry
 poetry init
+poetry add deepspeed==0.14.0
 poetry add "transformers==4.39.1" tiktoken einops transformers_stream_generator==0.0.5
 
 poetry add torchtext scikit-learn tensorboardX nltk numpy pandas sentencepiece scipy protobuf jieba rouge-chinese nltk uvicorn pydantic fastapi sse-starlette matplotlib
