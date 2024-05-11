90db0d603cebed6541440ca9f2b5c2a956b945f0
AmbroseX <rongkangxiong@idea.edu.cn>
2024-05-11 11:15:23 +0800

    streaming loader

--- src/data/transformer_loader.py
+++ src/data/transformer_loader.py
@@ -5,18 +5,73 @@ from tqdm import tqdm
 import time
 from datetime import timedelta
 import csv
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, IterableDataset
+import numpy as np
+from tqdm import tqdm
+import json
+
 from data.common import DatasetConfig
 from data.utils_multi import get_multi_hot_label, get_multi_list_label
 
 from extras.loggings import get_logger
 from models.component.common import ModelConfig
-from utils import read_data
+from utils import count_lines_in_jsonl_file, read_data
 logger = get_logger(__name__)
 
 
 PAD, CLS = '[PAD]', '[CLS]'  # padding符号, bert中综合信息符号, SEP并非必须
 
+
+class DynamicDataset(IterableDataset):
+    def __init__(self, path: str, tokenizer: "torch.nn.module", tag: str, pad_size: int, 
+                 max_samples: int, num_classes: int, multi_class: bool, multi_label: bool):
+        self.path = path
+        self.tokenizer = tokenizer
+        self.tag = tag
+        self.pad_size = pad_size
+        self.max_samples = max_samples
+        self.num_classes = num_classes
+        self.multi_class = multi_class
+        self.multi_label = multi_label
+        self.logger =get_logger(f"load:{tag}")
+        self.lines = count_lines_in_jsonl_file(self.path)
+
+    def __len__(self):
+        return self.lines
+
+    def __iter__(self):
+        flag = 0
+        line_num = 0
+        with open(self.path, 'r', encoding='UTF-8') as f:
+            self.logger.info(f"Loading {self.tag} dataset from \n{self.path}")
+            for line in tqdm(f):
+                if self.max_samples is not None and line_num >= self.max_samples:
+                    break
+                # Assuming each line in the file is a JSON string
+                data = json.loads(line)
+                content, label = data['text'], data['label']
+                if flag < 2:
+                    self.logger.info(f"content: {content}\nlabel: {label}")
+                    flag += 1
+
+                label = get_multi_list_label(label=label,
+                                             multi_class=self.multi_class,
+                                             multi_label=self.multi_label)
+                label = get_multi_hot_label(doc_labels=[label],
+                                            num_class=self.num_classes,
+                                            dtype=torch.float)[0]
+
+                inputs = self.tokenizer(content,
+                                        truncation=True,
+                                        return_tensors="pt",
+                                        padding='max_length',
+                                        max_length=self.pad_size)
+
+                yield (inputs['input_ids'].squeeze(0),
+                       inputs['attention_mask'].squeeze(0),
+                       inputs['token_type_ids'].squeeze(0), 
+                       label)
+                line_num += 1
 
 def load_dataset(path:str,
                  tokenizer:"torch.nn.module", 
@@ -82,31 +137,64 @@ def load_dataset(path:str,
     return batch_contents
 
 def build_dataset(config: "ModelConfig") -> tuple:
-    train = load_dataset(config.train_path, tag='training',
-                        tokenizer=config.tokenizer,
-                        pad_size = config.pad_size,
-                        max_samples = config.max_samples,
-                        num_classes = config.num_classes,
-                        multi_class = config.multi_class,
-                        multi_label = config.multi_label,
-                         )
-    val = load_dataset(config.val_path, tag = 'validation',
-                       tokenizer=config.tokenizer,
-                        pad_size = config.pad_size,
-                        max_samples = config.max_samples,
-                        num_classes = config.num_classes,
-                        multi_class = config.multi_class,
-                        multi_label = config.multi_label,
-                       )
-    test = load_dataset(config.test_path, tag = 'test',
-                        tokenizer=config.tokenizer,
-                        pad_size = config.pad_size,
-                        max_samples = config.max_samples,
-                        num_classes = config.num_classes,
-                        multi_class = config.multi_class,
-                        multi_label = config.multi_label,
+    if config.streaming:
+        train_dataset = DynamicDataset(
+            config.train_path, tag='training',
+            tokenizer=config.tokenizer,
+            pad_size = config.pad_size,
+            max_samples = config.max_samples,
+            num_classes = config.num_classes,
+            multi_class = config.multi_class,
+            multi_label = config.multi_label,
+        )
+
+        val_dataset = DynamicDataset(
+            config.val_path, tag = 'validation',
+            tokenizer=config.tokenizer,
+            pad_size = config.pad_size,
+            max_samples = config.max_samples,
+            num_classes = config.num_classes,
+            multi_class = config.multi_class,
+            multi_label = config.multi_label,
+        )
+        test_dataset = DynamicDataset(
+            config.test_path, tag = 'test',
+            tokenizer=config.tokenizer,
+            pad_size = config.pad_size,
+            max_samples = config.max_samples,
+            num_classes = config.num_classes,
+            multi_class = config.multi_class,
+            multi_label = config.multi_label,
+        )
+    else:
+        train_data = load_dataset(config.train_path, tag='training',
+                            tokenizer=config.tokenizer,
+                            pad_size = config.pad_size,
+                            max_samples = config.max_samples,
+                            num_classes = config.num_classes,
+                            multi_class = config.multi_class,
+                            multi_label = config.multi_label,
                             )
-    return train, val, test
+        val_data = load_dataset(config.val_path, tag = 'validation',
+                        tokenizer=config.tokenizer,
+                            pad_size = config.pad_size,
+                            max_samples = config.max_samples,
+                            num_classes = config.num_classes,
+                            multi_class = config.multi_class,
+                            multi_label = config.multi_label,
+                        )
+        test_data = load_dataset(config.test_path, tag = 'test',
+                            tokenizer=config.tokenizer,
+                            pad_size = config.pad_size,
+                            max_samples = config.max_samples,
+                            num_classes = config.num_classes,
+                            multi_class = config.multi_class,
+                            multi_label = config.multi_label,
+                                )
+        train_dataset = TransformerDataset(train_data)
+        val_dataset = TransformerDataset(val_data)
+        test_dataset =TransformerDataset(test_data)
+    return train_dataset, val_dataset, test_dataset
 
 class TransformerDataset(Dataset):
     def __init__(self, data):
